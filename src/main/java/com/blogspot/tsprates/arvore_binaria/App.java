bbe708b660896e9f62230d2ea78660dd1914f3fb
Thiago <tsprates@hotmail.com>
2016-02-18 13:25:19 -0200

    test travis

--- src/main/java/com/blogspot/tsprates/arvore_binaria/App.java
+++ src/main/java/com/blogspot/tsprates/arvore_binaria/App.java
@@ -1,13 +1,7 @@
 package com.blogspot.tsprates.arvore_binaria;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+	System.out.println("Árvore Binária");
     }
 }
